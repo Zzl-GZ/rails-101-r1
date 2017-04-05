afcf326cbd6f30308adc509696bed1b2d703b044
zzl <robinsonzhongziling@gmail.com>
2017-04-05 20:29:54 +0800

    implement my groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,15 +2,14 @@ class GroupsController < ApplicationController
 
    before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join , :quit]
    before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+   before_action :authenticate_user!
+
 
   def index
     @groups = Group.all
   end
 
-  def show
-    @group = Group.find(params[:id])
-    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
-  end
+
 
     def edit
     end
